9a3aa349429e6964eb454b2941762b6f1e7ba9df
TimothyKrajewski <krajewt1@tcnj.edu>
2017-12-04 16:46:04 -0500

    Made the board so everyone can test their parts

---
+++ FinalProject.scm
@@ -0,0 +1,13 @@
+#lang scheme
+(#%require r5rs/init)
+
+(define (board)
+  (vector
+     (list->vector '(X O X O X O X O))
+     (list->vector '(O X O X O X O X))
+     (list->vector '(X O X O X O X O))
+     (list->vector '(O X O X O X O X))
+     (list->vector '(X O X O X O X O))
+     (list->vector '(O X O X O X O X))
+     (list->vector '(X O X O X O X O))
+     (list->vector '(O X O X O X O X))))
